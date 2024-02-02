bfbece47e8c30eaf75498b45863ef64e10488156
keshika-dabidin-audam <keshika.dabidin@gmail.com>
2024-02-02 10:42:50 +0100

    user class reduit avec repository

--- src/main/java/fr/kass/ittraining/model/User.java
+++ src/main/java/fr/kass/ittraining/model/User.java
@@ -1,4 +1,40 @@
 package fr.kass.ittraining.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nom;
+
+    @NotBlank
+    private String prenom;
+
+    private String nomEntreprise;
+
+    @NotBlank
+    private Integer telephone;
+
+    @NotBlank
+    @Email
+    private String email;
+
 }
