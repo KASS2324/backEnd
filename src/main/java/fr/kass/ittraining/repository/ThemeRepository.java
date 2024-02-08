78b0769a47995b4da36d62a626d821ec5eed2883
antoinefrin <antoinefrindev@gmail.com>
2024-02-08 12:18:15 +0100

    Simplification de la recherche par theme
    requete sql plus spécifique dans formationService et suppression des fichier theme qui devienne obsolete

--- src/main/java/fr/kass/ittraining/repository/ThemeRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.kass.ittraining.repository;
-
-import fr.kass.ittraining.model.Theme;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ThemeRepository extends JpaRepository<Theme, Long> {
-}
